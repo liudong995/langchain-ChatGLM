153ea8ad6443b00b4a0f1bc148e0dbb6ed2c56dc
liudong_123 <1769583645@qq.com>
2023-09-15 11:33:12 +0800

    去除聊天返回文件的url

--- server/chat/knowledge_base_chat.py
+++ server/chat/knowledge_base_chat.py
@@ -78,7 +78,7 @@ def knowledge_base_chat(query: str = Body(..., description="用户输入", examp
             if local_doc_url:
                 url = "file://" + doc.metadata["source"]
             else:
-                parameters = urlencode({"knowledge_base_name": knowledge_base_name, "file_name":filename})
+                parameters = urlencode({"knowledge_base_name": knowledge_base_name, "file_name": filename})
                 url = f"{request.base_url}knowledge_base/download_doc?" + parameters
             text = f"""出处 [{inum + 1}] [{filename}]\n\n{doc.page_content}\n\n"""
             source_documents.append(text)
